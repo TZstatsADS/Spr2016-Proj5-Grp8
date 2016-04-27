6c78781755fc7a131face7d7a8ea26c6513482d7
jpchamps <juanpablo.camposg>
2016-04-27 01:37:40 -0400

    final tweets

--- lib/twitter_collections.R
+++ lib/twitter_collections.R
@@ -19,10 +19,11 @@ tweets.in.collection <- function(col.id){
   }
   return(id.current.events)
 }
+tweets.id <- final.events$id
 post.tweets <- function(tweets.id,col.id){
   for (i in 1:length(tweets.id)){
     tweet.id <- tweets.id[i]
-    post.tweet <- POST(paste0("https://api.twitter.com/1.1/collections/entries/add.json?tweet_id=",tweets.id,"&id=",col.id),config(token=twitter_token))
+    post.tweet <- POST(paste0("https://api.twitter.com/1.1/collections/entries/add.json?tweet_id=",tweet.id,"&id=",col.id),config(token=twitter_token))
     temp <- content(post.tweet, as = "text")
     temp <- fromJSON(temp)
   }
@@ -110,17 +111,16 @@ load("/Users/JPC/Documents/R/twitter API/collections_ids.RData")
 ###   Suggested Events   ###
 #___________________________#
 
-
+# delete current tweets
+# delete.tweets(tweets.in.collection(id.col.dsevents),id.col.dsevents)
+tweets.in.collection(id.col.dsevents)
 # Add tweets to the list:
-must <- 
 
-
-delete.events <- c("721974701191479296","722057049094418432","722403536567345152","722817347451543552")
+delete.events <- c("723239750941822976","723787643176259584","724992968642453504","724868193043529728","724469270223151104","721974701191479296","722057049094418432","722403536567345152","722817347451543552")
 index.delete.events <- which(tweets.events$id %in% delete.events)
 final.events <- tweets.events[-1*index.delete.events,]
-post.tweets(tweets.events$id[12],id.col.dsevents)
-post.tweet(tweets.events$id[15],id.col.dsevents)
-post.tweet(tweets.events$id[3],id.col.custevents)
+dim(final.events)
+post.tweets(rev(final.events$id),id.col.dsevents)
 
 # Delete tweets
 delete.tweet(tweets.events$id[12],id.col.dsevents)
@@ -131,16 +131,36 @@ delete.tweet(tweets.events$id[12],id.col.dsevents)
 post.tweet(tweets.events$id[1],id.col.dsevents)
 delete.tweet(tweets.events$id[4],id.col.custevents)
 
-all.tweets <- tweets.in.collection(id.col.dsevents)
+
 
 ###   Suggested Articles   ###
 #___________________________#
 
-# Add tweets to the list:
+# delete current tweets
+# delete.tweets(tweets.in.collection(id.col.dsarticles),id.col.dsarticles)
+tweets.in.collection(id.col.dsarticles)
 
-must <- c("722566794347589632","722111371362594816")
-pp[[2]]
-delete <- ("722846456265060356")
+1
+2
+3
+7
+9
+12
+13
+14
+15
+19
+22
+23
+pp[22]
+must.articles <- c("722566794347589632","722111371362594816")
+delete.articles <- c("722890699331346433","724542237430915077","724597255290925056","723967944426921985","722846456265060356","724481351701921792","722359900819570688","724360358941589508","722697270571986944","722757481106046976","722778791286870017")
+index.delete.articles <- which(tweets.articles.red$id %in% delete.articles)
+final.articles <- tweets.articles.red[-1*index.delete.articles,]
+dim(final.articles)
+length(final.articles$id)
+length(unique(final.articles$id))
+post.tweets(rev(unique(c(must.articles,final.articles$id))),id.col.dsarticles) 
 
 ###   Customized Articles   ###
 #___________________________#
